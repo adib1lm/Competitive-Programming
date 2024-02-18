f89ec3dc233c7ae04e87d67ad6bb748238f856b2
adib1lm <142324886+adib1lm@users.noreply.github.com>
2024-02-18 15:15:42 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1758C | Almost All Multiples/182648801.cpp
@@ -0,0 +1,186 @@
+#include <iostream>  
+#include<sstream> 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+//----------------------------------------------defs------------------------------------------------------------------------------------------------------//
+#define   NeedForSpeed  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define   int           long long int
+#define   uint          unsigned int
+#define   pi            pair<int,int>
+#define   all(x)        (x).begin(), (x).end()
+#define   rep(i, l, r)  for(int i=(int)(l);i<(int)(r);i++)
+#define   repd(i, l, r) for (int i=(int)(l);i>=(int)(r);i--)
+#define   clrg(i, l, r) for(int i=(int)(l);i<(int)(r);i++)vis[i]=0,v[i].clear();
+#define   print2d(mat,n,m){for(int i=0;i<(int)(n);i++){for(int j=0;j<(m);j++){cout<<mat[i][j]<<" ";}cout<< endl;}}
+#define   clr(a,x)      memset(a,x,sizeof(a))
+#define   rr(v)         for(auto &val:v)
+#define   print(v)      for (const auto itr : v){cout<<itr<<' ';}cout<<"\n";
+#define   len           length()
+#define   sz            size()
+#define   elif          else if
+#define   filein(x)     ifstream fin (x);
+#define   fileout(x)    ofstream fout (x);
+#define   nline         "\n"
+#define   pub           push_back
+#define   ppb           pop_back
+#define   mp            make_pair
+#define   ff            first
+//#define   ss            second
+#define   set_bits      __builtin_popcountll
+#define   mod           1000000007
+#define   mod1          998244353
+#define   INF           1e18
+//----------------------------------------------funcs--------------------------------------------------------------------------//
+int power(int x, unsigned int y){int res = 1;while (y > 0){ if (y & 1){res = res*x;} y = y>>1;x = x*x;}return res;}
+int powermod(int x, unsigned int y, int p){int res = 1;x = x % p;while (y > 0){if (y & 1){res = (res*x) % p;}y = y>>1; x = (x*x) % p;}return res;}
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+ 
+//-----------------------------------------debug----------------------------------------------------------//
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+//void _print(long long t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+ 
+// Type Cnversions//
+string toString(int _i){stringstream ss;ss<<_i;string _s;ss>>_s;return _s;}
+string ftoString(float _f){ostringstream oss;oss<<_f;string _s;_s=oss.str();return _s;}
+int s2i(string _s){stringstream ss(_s);int _i; ss>>_i;return _i;}
+int n2i(char _c){ return (int)_c - '0';} // for number chars
+int a2i(char _c){ return (int)_c;} // for alpha chars
+string stringSplit(string _s, int _i, int _j){return _s.substr(_i, _j-_i);}//splitting string from i to j//
+ 
+ 
+ //for printing//
+#define yes "Yes"
+#define no  "No"
+#define YES "YES"
+#define NO  "NO"
+#define printreturn(x) {cout<<x<<nline; return 0;}
+#define continue continue
+#define  vitr vector<int>::iterator
+ 
+ 
+//for triversing//
+#define fauto(x,v) for(auto& x:v)
+ //sum shortcuts//
+ int nSum (int _n){return (_n*(_n+1))/2;}
+ 
+ //usaco input//
+ void USACOsetIO(string s) {
+    freopen((s + ".in").c_str(), "r", stdin);
+    freopen((s + ".out").c_str(), "w", stdout);
+}
+ 
+//factorization func//
+vector<int> trial_factor(int n) {
+    vector<int> factorization;
+    for (int d = 2; d * d <= n; d++) {
+        while (n % d == 0) {
+            factorization.push_back(d);
+            n /= d;
+        }
+    }
+    if (n > 1)
+        factorization.push_back(n);
+    return factorization;
+}
+ //templates//
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//@adib_1lm//---------------------------------------XXXXXXXXXXXXXX-------------------------------------------------//@adib_1lm//
+//----------------------------------------------------@adib_1lm--------------------------------------------------------------//
+ 
+bool isPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+ 
+    return true;
+}
+void initCode(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.in", "r", stdin);
+        freopen("output.out", "w", stdout);
+        freopen("error.err", "w", stderr);
+    #endif
+    NeedForSpeed
+ }
+int solve(){
+    int n , x;
+ 
+    cin>>n>>x;
+    int arr[n] = {0};
+    for (int i = 0; i <n; ++i)
+    {
+        arr[i] = i+1;
+    }
+    if(n==x){
+        swap(arr[0],arr[n-1]);
+        print(arr);
+        return 0;        
+    }
+ 
+    if(n%x !=0){
+        printreturn(-1);
+    }
+ 
+    //initing//
+    arr[0] = x;
+    arr[n-1] = 1;
+    arr[x - 1] = n;
+    //print(arr);
+    //swaping on multiples//
+    int curri = x - 1;
+ 
+    for (int i = x; i < n-1; ++i)
+    {
+        if(n%(i+1)==0 and n%arr[i]==0 and arr[i]!=n and arr[i]%(curri+1) == 0){
+            //cout<<i<<endl;
+            swap(arr[curri],arr[i]);
+            curri = i;
+            continue;
+        }
+    }
+ 
+    print(arr);
+    return 0;
+}
+ 
+int32_t main() {
+    //for sublime and all//
+    initCode();
+    //for usaco//
+    //USACOsetIO("");
+ 
+    int t = 1;
+    cin>>t;
+    while(t--){
+        
+        solve();
+        //cout<<solve()<<endl;
+    }
+}
